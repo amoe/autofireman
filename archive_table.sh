1ff4399454915688404e2071c993f7cd0a77cf95
David Banks <amoebae@gmail.com>
2019-06-28 10:55:52 +0100

    more safety

--- archive_table.sh
+++ archive_table.sh
@@ -17,10 +17,37 @@ fi
 
 archive_prefix=/archive/pg
 output_file_name="${database}.${table}.sql.gz"
+final_output_file="${archive_prefix}/${output_file_name}"
 
 
-pg_dump -U postgres -d "$database" -t "$table" \
+echo "Dumping"
+pg_dump -U postgres -d "$database" -t "\"$table\"" \
   | pv \
-  | gzip -c9 > "${archive_prefix}/${output_file_name}"
+  | gzip -c9 > "$final_output_file"
 
-printf 'DROP TABLE "%s"' "$table" | psql -U postgres "$database"
+echo "Test uncompress."
+
+length=$(zcat "$final_output_file" | pv | wc -c)
+if [ "$length" -le 0 ]; then 
+    echo "Zero length dump?  BAILING!" >&2
+    exit 1
+fi
+
+echo "Uncompressed length is $length"
+ls -lh "$final_output_file"
+
+read -p "Drop table (y/n)? " choice
+case "$choice" in 
+    y|Y)
+        printf 'DROP TABLE "%s"' "$table" | psql -U postgres "$database"
+        ;;
+    n|N) 
+        echo "Exiting without drop."
+        ;;
+    *) 
+        echo "invalid"
+        ;;
+esac
+
+
+
